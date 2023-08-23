789abb19ac3cead863ba75c49a52c94d7ef7bfba
LeeChoongHwan <choo901@naver.com>
2023-08-23 15:57:57 +0900

    [FEAT] : ⚙️ Create Product Data class

---
+++ app/src/main/java/com/example/android_practice_applamarket/data/Product.kt
@@ -0,0 +1,9 @@
+package com.example.android_practice_applamarket.data
+
+data class Product(
+    val name : String,
+    val location : String,
+    val price : Int,
+    val reply : Int,
+    val like : Int
+)
