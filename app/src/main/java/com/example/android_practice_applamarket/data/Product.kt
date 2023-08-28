18daef57e7cb6044ba1d877f2001d8b689a661ce
LeeChoongHwan <choo901@naver.com>
2023-08-28 16:21:03 +0900

    [FEAT] : ⚙️ Add Parcelize

--- app/src/main/java/com/example/android_practice_applamarket/data/Product.kt
+++ app/src/main/java/com/example/android_practice_applamarket/data/Product.kt
@@ -1,9 +1,17 @@
 package com.example.android_practice_applamarket.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Product(
+    val image : String,
+    val source : Int,
     val name : String,
-    val location : String,
+    val description : String,
+    val owner : String,
     val price : Int,
-    val reply : Int,
-    val like : Int
-)
+    val location : String,
+    val like : Int,
+    val reply : Int
+) : Parcelable
