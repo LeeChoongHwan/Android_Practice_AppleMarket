ba86d82f38aa7408a3f69ce59b5877308b0cd7ca
LeeChoongHwan <choo901@naver.com>
2023-08-29 21:25:54 +0900

    [FEAT] : ⚙️ Feature Detail Layout

---
+++ app/src/main/java/com/example/android_practice_applamarket/DetailContentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_practice_applamarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_content)
+    }
+}
